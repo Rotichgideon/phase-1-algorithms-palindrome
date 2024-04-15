704f26af496ee789f70fb145d8cbd381813a4240
gideon rotich <gideonrotich2001@gmail.com>
2024-04-15 17:40:59 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let re = /[^A-Za-z0-9]/g;
+  reverseWord = word.toLowerCase().replace(re, "").split("").reverse().join("")
+  if (reverseWord === word) {
+    return true;
+  } else {
+    return false;
+  };
 }
 
 /* 
   Add your pseudocode here
+  let word = "mom"
+
+  function isPalindrome(word) {
+    re = /[^A-Za-z0-9]/g;
+    reverseWord = word.toLowerCase().replace(re, "").split("").reverse("").join("")
+  return reverseWord === word = true;
+  return false if reverseWord !== word;
+  }
 */
 
 /*
   Add written explanation of your solution here
+
+  RgeExp is used to remove unwanted characters from word passed;
+    means it matches anything that is not enclosed between A-za-z0-9_
+  change our word into lowecase, return a new string with matching patterns - use the regexp, split into an array, reverse them and later join the reversed new array
+  check whether the two words are similar, and if so return true else return false
 */
 
 // You can run `node index.js` to view these console logs
